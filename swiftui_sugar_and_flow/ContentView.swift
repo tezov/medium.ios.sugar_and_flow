b0c9363debe8aa3a5cc785152c048c9fe3e85bc1
tezov <tezov.app@gmail.com>
2023-11-13 18:17:20 +0100

    Initial Commit

---
+++ swiftui_sugar_and_flow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftui_sugar_and_flow
+//
+//  Created by prive on 13/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftui_sugar_and_flow/swiftui_sugar_and_flowApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_sugar_and_flowApp.swift
+//  swiftui_sugar_and_flow
+//
+//  Created by prive on 13/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_sugar_and_flowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
